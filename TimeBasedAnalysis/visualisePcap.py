4d75e2eeebc013ce158eaf3e055a9e6abcf7a85f
DKarandikar <daniel.karandikar@zoho.com>
2018-05-23 11:21:07 +0100

    PcapVisualisation
    Slight changes to visualisation script and recording results from Hue

--- TimeBasedAnalysis/visualisePcap.py
+++ TimeBasedAnalysis/visualisePcap.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 pkts = pyshark.FileCapture(os.path.join(os.path.dirname(os.path.abspath(__file__)), "pcaps", "HueOnOff1.pcap"))
 
 DEVICE_IP = "192.168.4.16"
+PHONE_IP = "192.168.4.19"
+
+IGNORE_PHONE = True
 
 initialTime = float(pkts[0].sniff_timestamp)
 
@@ -17,12 +20,13 @@ for p in pkts:
     if 'IP' in p:
         #print(p.sniff_timestamp)
         try:
-            if p['ip'].src == DEVICE_IP:
-                outTimes.append(float(p.sniff_timestamp) - initialTime)
-                outSize.append(int(p.length))
-            else:
-                inTimes.append(float(p.sniff_timestamp) - initialTime)
-                inSize.append(int(p.length))
+            if (IGNORE_PHONE and p['ip'].src != PHONE_IP and p['ip'].dst != PHONE_IP) or not IGNORE_PHONE:
+                if p['ip'].src == DEVICE_IP:
+                    outTimes.append(float(p.sniff_timestamp) - initialTime)
+                    outSize.append(int(p.length))
+                else:
+                    inTimes.append(float(p.sniff_timestamp) - initialTime)
+                    inSize.append(int(p.length))
         except AttributeError:
             print("Attribute Error")
 
