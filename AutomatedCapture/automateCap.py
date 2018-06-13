e656ac1e5ab6db2616ca4523ad7ffe142f6f7ba0
DKarandikar <daniel.karandikar@zoho.com>
2018-06-13 10:07:13 +0000

    CaptureSuccessLinux

--- AutomatedCapture/automateCap.py
+++ AutomatedCapture/automateCap.py
@@ -8,6 +8,7 @@ from subprocess import call, Popen, PIPE, getoutput
 
 INTERFACE_NAME = "wlan0"
 DEVICE_IP = "192.168.4.2"
+SESSION_LENGTH = 100 # Max seconds to play
 FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -34,11 +35,11 @@ def convertFloatLength(string):
     seconds = string.split(":")[2].split(".")[0]
     return (3600*int(hours) + 60*int(minutes) + int(seconds) + 1)
 
-def playForLess20Mins(filename):
+def playForLessXMins(filename):
     fileLength = getFileLength(filename)
     lengthFloat = convertFloatLength(fileLength)
-    numberPlays = math.floor(1.0* 1200 / float(lengthFloat))
-    print("File length is " + str(int(lengthFloat)) + " seconds, playing " + numberPlays + " times" )
+    numberPlays = math.floor(1.0* SESSION_LENGTH / float(lengthFloat))
+    print("File length is " + str(int(lengthFloat)) + " seconds, playing " + str(numberPlays) + " times" )
     t = threading.Thread(target=loopSong, args=(filename, numberPlays ) , name="PlayMusic")
     t.start()
 
@@ -74,11 +75,11 @@ def main():
             
             fullPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "audioFiles", file)
 
-            time = playForLess20Mins(fullPath)
+            time = playForLessXMins(fullPath)
 
-            print("Capturing for " + time + 15 + " seconds")
+            print("Capturing for " + str(time + 15) + " seconds")
 
-            packets = sniff(filter="ip " + DEVICE_IP , timeout=time + 15, iface=INTERFACE_NAME)
+            packets = sniff(filter="ip " + DEVICE_IP , timeout=time + 15, iface=INTERFACE_NAME, prn=lambda pkt: pkt.summary())
 
             savePackets(packets, file.split(".")[0])
 
